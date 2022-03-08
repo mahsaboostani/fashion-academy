b8ad88c838ceb9dba13e553cb9875e334d0be3ea
Mahsa Boostani <mahsaboostani@Mahsas-MacBook-Pro.local>
2022-03-08 20:46:17 +0100

    add a color picker

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
+
+
+
 let emailAddress = document.getElementById("exampleDropdownFormEmail1");
 let passWord = document.getElementById("exampleDropdownFormPassword1");
 let errOne = document.getElementById("error1");
 let errTwo = document.getElementById("error2");
 let wel = document.getElementById("welcome");
+
+let black = document.getElementsByClassName("bg-dark");
+let yelow = document.getElementsByClassName("bg-warning");
+
+let arroW = document.getElementById("arrow");
+
+
 
 
 
@@ -22,6 +32,36 @@ function check(event) {
 }
 
 
+function changeColor(event) {
+    var color = event.value;
+    for (let i = 0; i < black.length; i++) {
+        black[i].style.setProperty('background-color', color, 'important');
+    }
+
+
+}
+
+function changeColorY(event) {
+    var color = event.value;
+    for (let i = 0; i < yelow.length; i++) {
+        yelow[i].style.setProperty('background-color', color, 'important');
+    }
+
+
+}
+
+
+function activeArrow() {
+    let chan = document.getElementById("change");
+    if (chan.classList.contains("change-hide")) {
+        chan.classList.remove("change-hide");
+        chan.classList.add("change-box");
+    } else {
+        chan.classList.add("change-hide");
+        chan.classList.remove("change-box");
+    }
+}
+
 
 document.getElementById('mybtn').addEventListener("click", check);
-//document.getElementById("myDropdown").classList.toggle("show");
+
